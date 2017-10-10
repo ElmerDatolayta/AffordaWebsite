24dce7955be8528f79c5934a97673ce58b51fd82
Melvin Reston <melvin@creativedevlabs.com>
2017-10-10 18:18:56 +0800

    Products Section

--- script/script.js
+++ script/script.js
@@ -19,3 +19,19 @@ $('#productSlick2').slick({
   slidesToShow: 1,
   slidesToScroll: 1
 });
+
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: false,
+  focusOnSelect: true
+});
