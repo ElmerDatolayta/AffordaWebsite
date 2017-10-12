d3d365ee51c089f5a26cf3de2a0fed3f8db3ae6c
DESKTOP-O6M8492\Elmer Datolayta <e.datolaytz@gmail.com>
2017-10-12 21:43:13 +0800

    Updateed Design

--- script/script.js
+++ script/script.js
@@ -51,3 +51,20 @@ $('.slider-nav').slick({
 
 $('.single-item').slick();
 
+
+$('a.scroll-nav').click(function(e){
+  // prevent default action
+  e.preventDefault();
+  
+  // get id of target div (placed in href attribute of anchor element)
+  // and pass it to the scrollToElement function
+      // also, use 2000 as an argument for the scroll speed (2 seconds, 2000 milliseconds)
+  scrollToElement( $(this).attr('href'), 1000 );
+  });
+
+  var scrollToElement = function(el, ms){
+      var speed = (ms) ? ms : 600;
+      $('html,body').animate({
+          scrollTop: $(el).offset().top
+      }, speed);
+  };
