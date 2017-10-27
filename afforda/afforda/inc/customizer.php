ba3ea8609569ef6943849d88727f52b14a08c12a
Francis <francis@creativedevlabs.com>
2017-10-27 19:28:24 +0800

    Changed the whole index file.

---
+++ afforda/afforda/inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Afforda Theme Customizer
+ *
+ * @package Afforda
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function afforda_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'afforda_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'afforda_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'afforda_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function afforda_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function afforda_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function afforda_customize_preview_js() {
+	wp_enqueue_script( 'afforda-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'afforda_customize_preview_js' );
--- script/script.js
+++ script/script.js
@@ -1,5 +1,14 @@
 'use strict';
 
+$(window).scroll(function() {
+    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
+    {
+        $('.opaque-navbar').addClass('opaque');
+    } else {
+        $('.opaque-navbar').removeClass('opaque');
+    }
+});
+
 $('#bannerSlick').slick({
   autoplay:true,
   infinite: true,
@@ -104,26 +113,34 @@ $('.for-tablet').slick({
   focusOnSelect: true
 });
 
-function openCity(evt, cityName) {
-      // Declare all variables
-      var i, tabcontent, tablinks;
+$('.product-carousel').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+})
 
-      // Get all elements with class="tabcontent" and hide them
-      tabcontent = document.getElementsByClassName("tabcontent");
-      for (i = 0; i < tabcontent.length; i++) {
-          tabcontent[i].style.display = "none";
-      }
+// function openCity(evt, cityName) {
+//       // Declare all variables
+//       var i, tabcontent, tablinks;
 
-      // Get all elements with class="tablinks" and remove the class "active"
-      tablinks = document.getElementsByClassName("tablinks");
-      for (i = 0; i < tablinks.length; i++) {
-          tablinks[i].className = tablinks[i].className.replace(" active", "");
-      }
+//       // Get all elements with class="tabcontent" and hide them
+//       tabcontent = document.getElementsByClassName("tabcontent");
+//       for (i = 0; i < tabcontent.length; i++) {
+//           tabcontent[i].style.display = "none";
+//       }
 
-      // Show the current tab, and add an "active" class to the button that opened the tab
-      document.getElementById(cityName).style.display = "block";
-      evt.currentTarget.className += " active";
-  }
+//       // Get all elements with class="tablinks" and remove the class "active"
+//       tablinks = document.getElementsByClassName("tablinks");
+//       for (i = 0; i < tablinks.length; i++) {
+//           tablinks[i].className = tablinks[i].className.replace(" active", "");
+//       }
+
+//       // Show the current tab, and add an "active" class to the button that opened the tab
+//       document.getElementById(cityName).style.display = "block";
+//       evt.currentTarget.className += " active";
+//   }
     // Get the element with id="defaultOpen" and click on it
-    document.getElementById("defaultOpen").click();
+    // document.getElementById("defaultOpen").click();
 
+      
